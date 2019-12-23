07a76467d55e993ec9ef9292ca03718418a164f4
Ji1998 <49043409+Ji1998@users.noreply.github.com>
2019-12-23 17:19:02 -0500

    Update Reverse_Integer.cpp

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -3,10 +3,10 @@ public:
     int reverse(int x) {
         long rev = 0;
         while (x){
-            rev = rev*10 + x%10;
-            x = x/10;
+            rev = rev*10 + x%10;            //  加号前面先取得的值，保持最高位。 加号后面取得个位数
+            x = x/10;                       //裁剪原数，确保从低位到高位都能被依次取得
         }
-        if (rev < INT_MIN || rev > INT_MAX){
+        if (rev < INT_MIN || rev > INT_MAX){                
             return 0;
         }
         else {
