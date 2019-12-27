96400e55b922210ddf8be541f3c6c1b0fb64794c
Ji1998 <49043409+Ji1998@users.noreply.github.com>
2019-12-27 12:14:09 -0500

    Update Merge Two Lists.cpp

--- Merge Two Lists.cpp
+++ Merge Two Lists.cpp
@@ -1,3 +1,7 @@
+//比较两个list中node的数值大小，然后插入新的list里边
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,9 +15,9 @@ public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *dummy = new ListNode(-1);
         ListNode *cur = dummy;
-        while (l1 && l2){
-            if(l1->val < l2->val){
-                cur->next = new ListNode (l1->val);
+        while (l1 && l2){                                           //考虑到l1和l2的长度可能不一样，如果其中一个更长
+            if(l1->val < l2->val){                                  //则会有NULL，因此不能true，跳出while loop
+                cur->next = new ListNode (l1->val);                 //把其剩余部分直接放入新list的尾部
                 l1 = l1->next;
             }
             else {
@@ -24,8 +28,8 @@ public:
             
             
         }
-            cur ->next = l1? l1:l2;
-            return dummy->next;
+            cur ->next = l1? l1:l2;                             //while loop走完之后，把多出来的部分接入新list尾部
+            return dummy->next;                                 //如果只return dummy，-1也会出现在新list，因此return dummy->next
         
             
         }
